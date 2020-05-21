c90288ae73233cc042040f44805260d5a8055221
DallasLind <mcgraw.dallas@gmail.com>
2020-05-21 17:59:00 -0400

    Adding a new class and method to add grades

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,24 +7,20 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+            var book = new Book();
+            book.AddGrade(89.1);
+ 
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
-
-            var result = 0.0;
+            
+             var result = 0.0;
             foreach(var number in grades) {
                 result += number;
             }
             result /= grades.Count;
             Console.WriteLine($"The average grade is {result:N1}");
 
-            if(args.Length > 0) {
-                Console.WriteLine($"Hello, {args[0]}!");
-                 }
-                 else {        
-            Console.WriteLine("Hello Bro!");
-            }
-         
-        }
     }
+    }
+
 }
