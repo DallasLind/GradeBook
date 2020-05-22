27596c75607fff7237f9b754e647b66141bece6f
DallasLind <mcgraw.dallas@gmail.com>
2020-05-22 16:57:14 -0400

    Finished making my unit tests and ran into a small error with my statistics method so ill go back shortly and check that out

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Book {
+    public class Book {
         public Book(string name) {
             grades = new List<double>();
             this.name = name;
@@ -13,20 +13,19 @@ namespace GradeBook
             grades.Add(grade);
         }
 
-        public void ShowStatistics() {
-             var result = 0.0;
-             var highGrade = double.MinValue;
-             var lowGrade = double.MaxValue;
+        public Statistics GetStatistics() {
+            var result = new Statistics();
+             result.Average = 0.0;
+             result.High = double.MinValue;
+             result.Low = double.MaxValue;
             foreach(var number in grades) {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
+                result.High = Math.Max(number, result.High);
+                result.Low = Math.Min(number, result.Low);
+                result.Average += number;
             }
-            result /= grades.Count;
-            Console.WriteLine($"The lowest grade is {lowGrade}");
-            Console.WriteLine($"The highest grade is {highGrade}");
-            Console.WriteLine($"The average grade is {result:N1}");
+            result.Average /= grades.Count;
 
+            return result;
         }
 
        private List<double> grades;
