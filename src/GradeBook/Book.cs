c062b6de5cdc186caf09a5f509d19dcc16737ce7
DallasLind <mcgraw.dallas@gmail.com>
2020-05-26 16:35:29 -0400

    Fixed some errors involving dotnet and added new test

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -6,22 +6,24 @@ namespace GradeBook
     public class Book {
         public Book(string name) {
             grades = new List<double>();
-            this.name = name;
+            Name = name;
         }
       public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
 
-        public Statistics GetStatistics() {
+        public Statistics GetStatistics() 
+        {
             var result = new Statistics();
              result.Average = 0.0;
              result.High = double.MinValue;
              result.Low = double.MaxValue;
-            foreach(var number in grades) {
-                result.High = Math.Max(number, result.High);
-                result.Low = Math.Min(number, result.Low);
-                result.Average += number;
+            
+            foreach(var grade in grades) {
+                result.High = Math.Max(grade, result.High);
+                result.Low = Math.Min(grade, result.Low);
+                result.Average += grade;
             }
             result.Average /= grades.Count;
 
@@ -29,6 +31,6 @@ namespace GradeBook
         }
 
        private List<double> grades;
-       private string name;
+       public string Name;
     }
 }
